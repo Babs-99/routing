751583093a474da83b986473b3c8e1c6ceec2d15
Babs <ababalola16@alustudent.com>
2024-03-19 13:07:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const routes = require('./route.js');
+
+app.use(express.json());
+
+app.use('/', routes);
+
+// const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log("Server is running......");
+});
